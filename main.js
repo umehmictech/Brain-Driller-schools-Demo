8b04f27e1ffdf5c9b55f4c541b4651a767ad8f22
umehmictech <iumeh059@gmail.com>
2024-07-27 03:39:11 +0100

    first demo commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// let age1 = 34;
+// let age2 = 36;
+
+// let ans = age1 + age2;
+// let ans2 = age1 * age2;
+// let ans3 = age1 ** age2;
+// let ans4 = age1 / age2;
+// let ans5 = age2 % age1
+// console.log("addition :" +  ans);
+// console.log("multiplication :" + ans2);
+// console.log("modulus :" + ans3);
+// console.log("division :" + ans4);
+// console.log(ans5)
